cc945b7c91f073b7fe077247b9f918539d2dc371
Jacob <Arthursjacob01@gmail.com>
2025-12-30 14:38:35 -0500

    Add slug field to tag model

--- app/models/tags.py
+++ app/models/tags.py
@@ -6,5 +6,6 @@ class Tag(Base):
     
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False, unique=True)
+    slug = Column(String(100), nullable=False, unique=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
